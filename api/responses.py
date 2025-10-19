c1705affc0546f66e4279970f54f8a966b1f03c2
Anshu <anshuvairagade7@gmail.com>
2025-10-19 14:49:13 +0530

    Pushing code to repo

--- api/responses.py
+++
@@ -1,7 +0,0 @@
-import orjson
-from starlette.responses import JSONResponse
-
-
-class ORJSONResponse(JSONResponse):
-    def render(self, content):
-        return orjson.dumps(content)
--- fmt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-PATHS="api app tests alembic"
-isort -q -rc $PATHS
-black -q $PATHS
-flake8 $PATHS
